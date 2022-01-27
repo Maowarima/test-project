87efa8b336844ed1100c85587b54989f258142e5
Maowarima <98450009+Maowarima@users.noreply.github.com>
2022-01-27 06:38:47 -0800

    Add files via upload

---
+++ 1st design/inddex.js
@@ -0,0 +1,25 @@
+/*
+const biyog =
+function   (x,y)
+
+{
+return
+
+*/
+
+ const square = function(x){
+
+    return x * y 
+ };
+
+ const borgo = x =>{
+    return x * x;
+ };
+
+ const add = (a,b ) =>{
+    return a+b;
+ };
+
+ const isEven = (x) {
+    return x % 2 === 0;
+ }
